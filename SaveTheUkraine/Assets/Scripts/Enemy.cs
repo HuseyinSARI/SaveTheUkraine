b0d487374ed8d23159c76537b4ba6ef1c4077ee6
Hüseyin <36958242+HuseyinSARI@users.noreply.github.com>
2022-03-09 17:19:44 +0300

    making the enemy look where he is going
    Quaternion.LookRotation

--- SaveTheUkraine/Assets/Scripts/Enemy.cs
+++ SaveTheUkraine/Assets/Scripts/Enemy.cs
@@ -24,24 +24,34 @@ public class Enemy : MonoBehaviour
         if (!canMoveRigth)
         {
             transform.position = Vector3.MoveTowards(transform.position, movePoints[0].position, speed * Time.deltaTime);
+            LookAtTheTarget(movePoints[0].position);
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, movePoints[1].position, speed * Time.deltaTime);
-
+            LookAtTheTarget(movePoints[1].position);
         }
     }
     private void CheckCanMoveRigth()
     {
         if(Vector3.Distance(transform.position,movePoints[0].position) <= 0.1f){
             canMoveRigth = true;
-            print("move rigth");
         }
         else if (Vector3.Distance(transform.position, movePoints[1].position) <= 0.1f)
         {
             canMoveRigth = false;
-            print("move left");
         }
     }
+
+    private void LookAtTheTarget(Vector3 newTarget)
+        {
+        Quaternion _quaOffset = Quaternion.AngleAxis(90, Vector3.up);
+
+
+        Quaternion targetLocation = Quaternion.LookRotation((newTarget - transform.position)) * _quaOffset;
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetLocation, speed * Time.deltaTime) ;
+
+        
+        }
 }
 
